7bb440796e4511fd31b0af47211831155ef31ad9
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-14 17:26:07 +0300

    add fingerprintjs

--- src/components/Home/Welcome.tsx
+++ src/components/Home/Welcome.tsx
@@ -1,48 +1,21 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import fp from '@fingerprintjs/fingerprintjs';
+
 export default function Welcome() {
   useEffect(() => {
     (() => {
-      const windowObj = window || global;
+      const fpPromise = fp.load();
 
-      // Count Browser window object keys
-      const windowObjCount = () => {
-        const keys = [];
-        for (const i in windowObj) {
-          keys.push(i);
-        }
-        return keys.length.toString(36);
-      };
-      // window obj and navigator aggregate
-      const pad = (str: string, size: number) => {
-        return (new Array(size + 1).join('0') + str).slice(-size);
-      };
-
-      // Browser mimiTypes and User Agent count
-      const navi = (
-        navigator.mimeTypes.length + navigator.userAgent.length
-      ).toString(36);
-      const padString = pad(navi + windowObjCount(), 4);
-      // Browser screen specific properties
-      const width = windowObj.screen.width.toString(36);
-      const height = windowObj.screen.height.toString(36);
-      const availWidth = windowObj.screen.availWidth.toString(36);
-      const availHeight = windowObj.screen.availHeight.toString(36);
-      const colorDepth = windowObj.screen.colorDepth.toString(36);
-      const pixelDepth = windowObj.screen.pixelDepth.toString(36);
-      // Base64 encode
-      console.log(
-        btoa(
-          padString +
-            width +
-            height +
-            availWidth +
-            availHeight +
-            colorDepth +
-            pixelDepth,
-        ),
-      );
+      // Get the visitor identifier when you need it.
+      fpPromise
+        .then((fp) => fp.get())
+        .then((result) => {
+          // This is the visitor identifier:
+          const visitorId = result.visitorId;
+          console.log(visitorId);
+        });
     })();
   });
 
