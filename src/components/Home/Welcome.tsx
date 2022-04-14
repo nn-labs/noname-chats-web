cd34eb8a9e60041c1e8a5b2c51fac72a1ba30901
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-14 17:13:20 +0300

    mf

--- src/components/Home/Welcome.tsx
+++ src/components/Home/Welcome.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Welcome() {
+  useEffect(() => {
+    (() => {
+      const windowObj = window || global;
+
+      // Count Browser window object keys
+      const windowObjCount = () => {
+        const keys = [];
+        for (const i in windowObj) {
+          keys.push(i);
+        }
+        return keys.length.toString(36);
+      };
+      // window obj and navigator aggregate
+      const pad = (str: string, size: number) => {
+        return (new Array(size + 1).join('0') + str).slice(-size);
+      };
+
+      // Browser mimiTypes and User Agent count
+      const navi = (
+        navigator.mimeTypes.length + navigator.userAgent.length
+      ).toString(36);
+      const padString = pad(navi + windowObjCount(), 4);
+      // Browser screen specific properties
+      const width = windowObj.screen.width.toString(36);
+      const height = windowObj.screen.height.toString(36);
+      const availWidth = windowObj.screen.availWidth.toString(36);
+      const availHeight = windowObj.screen.availHeight.toString(36);
+      const colorDepth = windowObj.screen.colorDepth.toString(36);
+      const pixelDepth = windowObj.screen.pixelDepth.toString(36);
+      // Base64 encode
+      console.log(
+        btoa(
+          padString +
+            width +
+            height +
+            availWidth +
+            availHeight +
+            colorDepth +
+            pixelDepth,
+        ),
+      );
+    })();
+  });
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h1 className="text-5xl font-bold text-neutral-300">Welcome</h1>
