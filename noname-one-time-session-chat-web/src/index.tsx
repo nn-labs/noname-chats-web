ea785986d83afab141f4f51ea15460284b0155f4
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-04-17 13:40:22 +0300

    refactoring

--- noname-one-time-session-chat-web/src/index.tsx
+++ noname-one-time-session-chat-web/src/index.tsx
@@ -4,18 +4,16 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { PageContextComp } from './contexts/PageContext';
-import { AuthContextComp } from './contexts/AuthContext';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
-  <AuthContextComp>
+
     <PageContextComp>
       <BrowserRouter>
-        {/*<React.StrictMode>*/}
+        <React.StrictMode>
         <App />
-        {/*</React.StrictMode>*/}
+        </React.StrictMode>
       </BrowserRouter>
-    </PageContextComp>
-  </AuthContextComp>,
+    </PageContextComp>,
 );
--- noname-one-time-session-chat-web/src/routes.tsx
+++ noname-one-time-session-chat-web/src/routes.tsx
@@ -5,47 +5,6 @@ import Chat from './components/Chat/Chat';
 import Welcome from './components/Home/Welcome';
 
 const routes = () => [
-  // {
-  //   path: '/chat',
-  //   element: isLoggedIn ? <Chat /> : <Navigate to="/login" />,
-  // },
-  //
-  // {
-  //   path: '/',
-  //   element: !isLoggedIn ? <Home /> : <Navigate to="/chat" />,
-  //   children: [
-  //     { path: 'login', element: <Login /> },
-  //     { path: '/', element: <Navigate to="/login" /> },
-  //   ],
-  // },
-
-  // {
-  //   path: '/',
-  //   element: !user ? <Home /> : <Welcome />,
-  // },
-  // {
-  //   path: '/chat',
-  //   // element: <Chat />,
-  //   element:
-  //     user && user.role === 'support' && !user['is_room'] ? (
-  //       <Welcome />
-  //     ) : (
-  //       <Chat />
-  //     ),
-  // },
-  // {
-  //   path: '/login',
-  //   element: !user ? <Login /> : <Navigate to="/" />,
-  // },
-  // {
-  //   path: '/registration',
-  //   element: !user ? <Registration /> : <Navigate to="/" />,
-  // },
-  // {
-  //   path: '*',
-  //   element: <Error />,
-  // },
-
   {
     path: '/',
     element: <Welcome />,
